8ed56b551efa6e49f75813d32c0d0e37c0543d67
Xiroe <10255101549@stu.ecnu.edu.cn>
2025-09-30 21:35:00 +0800

    Update main.c

--- C-getint-9.30/main.c
+++ C-getint-9.30/main.c
@@ -3,9 +3,9 @@
 #define SIZE 1000
 int main()
 {
-    int n,array[SIZE],getint(int *),s;
+    int n,array[SIZE],getint(int *);
 
-    for (n = 0; n < SIZE && (s = getint(&array[n])) != EOF;n++)
-        ;
-    printf("%i",s);
+    for (n = 0; n < SIZE &&  getint(&array[n]) != EOF;n++)
+        printf("%i",array[n]);
 }
+
